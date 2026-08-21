c72f60e51afc7db2ec2572e1549ee476bca3a76d
Utkarsh Shakya <iamutkarshakya@gmail.com>
2026-08-21 21:39:19 +0530

    Remove legacy server code and routes for Aryan Beauty Parlour project

--- legacy/server/routes/notificationRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { notificationSender } from "../controllers/notificationController.js";
-
-const router = express.Router();
-
-router.route("/send").post(notificationSender);
-
-export default router;
