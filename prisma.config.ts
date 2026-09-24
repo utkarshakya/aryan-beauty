3ca2fbf4042f0180f2a35e62eb9ede7048938198
Utkarsh Shakya <iamutkarshakya@gmail.com>
2026-09-24 18:49:59 +0530

    fix: correct datasource URL configuration in prisma.config.ts for application runtime

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,8 +4,8 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    // url: env("DATABASE_URL"), // Use DATABASE_URL for Application Runtime
-    url: env("DIRECT_URL"), // Use Direct URL for Migration and Seeding
+    url: env("DATABASE_URL"), // Use DATABASE_URL for Application Runtime
+    // url: env("DIRECT_URL"), // Use Direct URL for Migration and Seeding
   },
   migrations: {
     path: "prisma/migrations",
